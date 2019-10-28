3c2e3dc0de7d40dba87b5adf75ba7bc85cf99553
Rahul Reddy Dayam <42753261+rahulreddy062@users.noreply.github.com>
2019-10-27 19:10:43 -0500

    Update largestPrimeFactor.js

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,1 +1,10 @@
-
+let  leastPrime = 2;
+let  numbertoFind = 600851475143;
+while(numbertoFind > 1){
+    if(numbertoFind % leastPrime === 0){ 
+        numbertoFind /= leastPrime;
+    } else {
+        leastPrime++;
+    }
+}
+console.log(leastPrime);
